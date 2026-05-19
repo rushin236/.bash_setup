d3bd6f62180e30d01700fb5debcdaa534181822f
rushin236 <rushikeshshindesandesh@gmail.com>
2026-05-20 03:39:12 +0530

    added mise install support for alpine and fixed sourcing issues

--- pkgs.rc.d/52-starship.sh
+++ pkgs.rc.d/52-starship.sh
@@ -4,11 +4,11 @@ CACHE_DIR="$HOME/.cache/bash_setup"
 CACHE_FILE="$CACHE_DIR/starship.sh"
 
 # Ensure the cache directory exists (safeguard)
-makedirs "$CACHE_DIR"
+mkdir -p "$CACHE_DIR"
 
 # Only run the heavy binary if the cache file is missing
 if [[ ! -f "$CACHE_FILE" ]]; then
-  command -v starship >/dev/null  && starship init bash >"$CACHE_FILE"
+  command -v starship >/dev/null && starship init bash >"$CACHE_FILE"
 fi
 
 # Instantly read the static text file
